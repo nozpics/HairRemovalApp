6ee7fd919892f85ee51d04466e648a3c04eeb50d
nozpics <nozpi0707@gmail.com>
2024-11-26 11:43:41 +0900

    home画面のHTML&CSSを作成中

---
+++ src/main/java/com/example/hairremoval/web/WebThemeController.java
@@ -0,0 +1,5 @@
+package com.example.hairremoval.web;
+
+public class WebThemeController {
+
+}
--- src/main/resources/META-INF/com/example/hairremoval/dao/NextScheduleDao/selectByUserId.sql
+++ src/main/resources/META-INF/com/example/hairremoval/dao/NextScheduleDao/selectByUserId.sql
@@ -1,2 +1,5 @@
 -- 特定のユーザーIDの次回予定日を取得
 SELECT * FROM next_schedule WHERE user_id = /* userId */0
+
+-- 次回予定日を近い順に取得
+SELECT * FROM next_schedule ORDER BY next_date ASC;
