be5dd46326c8dbb52ca9759eebd02a929687fca3
nozpics <nozpi0707@gmail.com>
2024-11-26 16:27:00 +0900

    コントローラクラス作成中

--- src/main/java/com/example/hairremoval/web/HomeController.java
+++ src/main/java/com/example/hairremoval/web/HomeController.java
@@ -2,6 +2,7 @@ package com.example.hairremoval.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Webコントローラ
@@ -11,4 +12,13 @@ import org.springframework.stereotype.Controller;
 @Slf4j
 public class HomeController {
 
+  /**
+   * 脱毛管理画面のコントローラ
+   *
+   */
+
+  @GetMapping("/home")
+  public String get(String userName){
+    return "home";
+  }
 }
